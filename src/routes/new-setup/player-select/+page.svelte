c7f499de7f6bcf4b4e7609ebf3e8696cd51d1488
Dillon Smith <dsmith39@gmail.com>
2023-01-20 15:04:30 -0700

    styled the player-select page

--- src/routes/new-setup/player-select/+page.svelte
+++ src/routes/new-setup/player-select/+page.svelte
@@ -6,7 +6,14 @@
 
 <h2>How many people are playing?</h2>
 <form method="POST" action="?/create" use:enhance>
-	<input name="playerChoice" type="number" min="2" max="10" bind:value={playerChoice} />
+	<input
+		name="playerChoice"
+		type="number"
+		min="2"
+		max="10"
+		bind:value={playerChoice}
+		class="numberInput"
+	/>
 	{#if playerChoice >= 1}
 		<h3>Enter the nicknames of the players</h3>
 		{#each Array(playerChoice) as _, i}
@@ -18,3 +25,35 @@
 	{/if}
 	<button>Submit</button>
 </form>
+
+<style>
+	h2 {
+		font-size: 2rem;
+		margin: 5rem 0 1rem 0;
+	}
+	h3 {
+		font-size: 1.5rem;
+		margin: 1rem 0 1rem 0;
+	}
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	input {
+		font-size: 1.5rem;
+		margin: 0 0 1rem 0;
+	}
+	label {
+		display: block;
+		font-size: 1.5rem;
+		margin: 1rem 0 0.5rem 0;
+	}
+	button {
+		font-size: 1.5rem;
+		margin: 1rem 0 1rem 0;
+	}
+	.numberInput {
+		width: 5rem;
+	}
+</style>
